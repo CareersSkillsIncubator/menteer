84b2fd4ef480f3b7058176441dae26ae73c95ad3
Jason Hamilton-Mascioli <hamiltonmascioli@gmail.com>
2015-01-19 11:09:34 -0500

    update login message
    Signed-off-by: Jason Hamilton-Mascioli <hamiltonmascioli@gmail.com>

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -67,7 +67,7 @@ class Auth extends CI_Controller {
 
 				//if the login is successful
 				//redirect them back to the home page
-				$this->session->set_flashdata('message', $this->ion_auth->messages());
+				//$this->session->set_flashdata('message', $this->ion_auth->messages());
                 echo json_encode(array('vresult'=>'success','message'=>$this->ion_auth->messages()));
 
  			}
